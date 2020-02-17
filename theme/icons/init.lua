559318d8b99698f1be752082fb818e82fffd35a6
Reese <reesericci@gmail.com>
2020-02-17 17:25:18 -0600

    Update init.lua

--- theme/icons/init.lua
+++ theme/icons/init.lua
@@ -2,7 +2,7 @@ local dir = os.getenv('HOME') .. '/.config/awesome/theme/icons'
 
 return {
   --tags
-  internetexplorer = dir .. '/ie.svg',
+  chrome = dir .. '/ie.svg',
   code = dir .. '/code-braces.svg',
   social = dir .. '/forum.svg',
   folder = dir .. '/folder.svg',
