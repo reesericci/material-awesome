486a0723466cbefb28631d393582eb4214514c91
reesericci <reesericci@nyctech.cloud>
2020-07-12 07:52:35 -0500

    notifications!

--- module/notifications.lua
+++ module/notifications.lua
@@ -9,15 +9,15 @@ naughty.config.spacing = 8
 
 naughty.config.defaults.timeout = 5
 naughty.config.defaults.screen = 1
-naughty.config.defaults.position = 'bottom_left'
-naughty.config.defaults.margin = dpi(16)
+naughty.config.defaults.position = 'top_right'
+naughty.config.defaults.margin = dpi(20)
 naughty.config.defaults.ontop = true
-naughty.config.defaults.font = 'Roboto Regular 10'
+naughty.config.defaults.font = 'mononoki 10'
 naughty.config.defaults.icon = nil
 naughty.config.defaults.icon_size = dpi(32)
 naughty.config.defaults.shape = gears.shape.rounded_rect
 naughty.config.defaults.border_width = 0
-naughty.config.defaults.hover_timeout = nil
+naughty.config.defaults.hover_timeout = 5
 
 -- Error handling
 if _G.awesome.startup_errors then
